c33dc8b958b20d3f235630ac61128a69a25b4521
Shahan Weerakoon <email2shahan@gmail.com>
2025-04-26 23:40:07 +0530

    home page redirect to user login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,24 @@
 "use client";
 import {  useRouter } from "next/navigation";
+import { useEffect } from "react";
 // import { getSession, login, logout } from "@/lib";
 
-export default async function Page() {
+export default function Page() {
+  useEffect(() => {
+    redirectToLogin();
+  },[])
+
+  const router = useRouter();
+
+  const redirectToLogin = () => {
+    
+    router.push("/user/login");
+  }
   // const session = await getSession();
   // const route = session ? "/dashboard" : "/login";
-  const router = useRouter();
-  router.push("/user/login");
+
   
   return (
     <div></div>
-    // <section>
-    //   <form
-    //     action={async (formData) => {
-    //       "use server";
-    //       await login(formData);
-    //       redirect("/dashboard");
-    //     }}
-    //   >
-    //     <input name="email" type="email" placeholder="Email" />
-    //     <br />
-    //     <button type="submit">Login</button>
-    //   </form>
-    //   <form
-    //     action={async () => {
-    //       "use server";
-    //       await logout();
-    //       redirect("/");
-    //     }}
-    //   >
-    //     <button type="submit">Logout</button>
-    //   </form>
-    //   <pre>{JSON.stringify(session, null, 2)}</pre>
-    // </section>
   );
 }
